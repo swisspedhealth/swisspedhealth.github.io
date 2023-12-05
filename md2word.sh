ed50585024a769c29e17e4ef72503a128ef528f4
Dylan <dylan.lawless@protonmail.com>
2023-12-05 10:21:05 +0100

    Build site at 2023-12-05 09:21:05 UTC

--- md2word.sh
+++ md2word.sh
@@ -5,14 +5,12 @@ mkdir -p ./msword  # Create the msword directory if it doesn't exist
 for file in ./*.md; do
     filename=$(basename "$file" .md)  # Extract the base filename without extension
     pandoc "$file" -o "./msword/${filename}.docx"  # Convert to docx and output to msword directory
-done
-
+done &&
 
 
 # index file is md but html content
 
 cp index.md index.html
-pandoc index.html -o "./msword/index.docx"  # Convert to docx and output to msword directory
+pandoc index.html -o ./msword/index.docx # Convert to docx and output to msword directory
 rm index.html
-done
 
