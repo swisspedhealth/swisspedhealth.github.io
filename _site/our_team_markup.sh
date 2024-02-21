bd4eaf02a13761425ee02cbc9bda7e65ebe8d76f
Dylan <dylan.lawless@protonmail.com>
2024-02-21 10:37:00 +0100

    our team images

--- _site/our_team_markup.sh
+++ _site/our_team_markup.sh
@@ -5,7 +5,7 @@
 awk 'BEGIN {FS="\n"; RS=""}
      {
        print "  <div class=\"team-card\">"
-       print "    <img src=\"assets/images/medical-team-avatar/avatar_2.jpg\" alt=\"\" />"
+       print "    <img src=\"assets/images/our_team/avatar_2.jpg\" alt=\"\" />"
        print "    <h2>" $1 "</h2>"
        print "    <h3>" $2 "</h3>"
        print "    <p>" $3 "</p>"
