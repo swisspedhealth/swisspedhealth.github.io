44ffc60bfe0fd9c59e1779ebc99f3a2af5ce27a6
Dylan <dylan.lawless@protonmail.com>
2024-02-21 10:16:29 +0100

    Build site at 2024-02-21 09:16:29 UTC

---
+++ our_team_markup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!/bin/bash
+
+awk 'BEGIN {FS="\n"; RS=""}
+     {
+       print "  <div class=\"team-card\">"
+       print "    <img src=\"assets/images/medical-team-avatar/avatar_2.jpg\" alt=\"\" />"
+       print "    <h2>" $1 "</h2>"
+       print "    <h3>" $2 "</h3>"
+       print "    <p>" $3 "</p>"
+       print "  </div>"
+     }' our_team.txt > our_team_markup.txt
+
